f327dd42d012f7dc223d2d5085463e528326ecb0
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-11-17 12:17:55 +0700

    add some components, icons, layouts

---
+++ src/components/Fonts.tsx
@@ -0,0 +1,16 @@
+import { M_PLUS_Rounded_1c } from '@next/font/google'
+
+
+// const Fonts = () => (
+//   <style jsx global>{`
+//     @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap");
+//   `}</style>
+// );
+
+const Fonts = () => (
+  <style jsx global>{`
+    ${M_PLUS_Rounded_1c}
+  `}</style>
+)
+
+export default Fonts;
