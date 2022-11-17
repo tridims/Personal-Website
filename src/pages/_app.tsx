1cdc8319095770c1f702567365b68c4e7c7337c0
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-11-17 12:17:26 +0700

    Init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { trpc } from "../utils/trpc";
+
+import {ChakraProvider} from "@chakra-ui/react"
+import chakraTheme from "../lib/ChakraTheme"
+import Fonts from "../components/Fonts";
+
+import "../styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+  router
+}) => {
+  return (
+    <ChakraProvider theme={chakraTheme}>
+      <Fonts />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
