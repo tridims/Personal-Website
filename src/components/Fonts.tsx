b3a5c26d3cb83ed66b6da00a185397f88e5383b9
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2022-11-18 10:53:46 +0700

    revert fonts

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -1,16 +1,21 @@
-import { M_PLUS_Rounded_1c } from '@next/font/google'
-
-
-// const Fonts = () => (
-//   <style jsx global>{`
-//     @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap");
-//   `}</style>
-// );
+import { M_PLUS_Rounded_1c } from "@next/font/google";
+import { NextPage } from "next";
+import React from "react";
 
 const Fonts = () => (
   <style jsx global>{`
-    ${M_PLUS_Rounded_1c}
+    @import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap");
   `}</style>
-)
+);
 
 export default Fonts;
+
+const fontVar = M_PLUS_Rounded_1c({
+  style: "normal",
+  weight: ["300", "700"],
+  display: "swap",
+});
+
+// export default function Fonts({ children }: { children: React.ReactNode }) {
+//   return <main className={fontVar.className}>{children}</main>;
+// }
