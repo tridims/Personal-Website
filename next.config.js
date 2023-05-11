ad1b8fb9e023ccb2a1bd2b026b6de571edb32403
tridims <tri.dimas@student.ub.ac.id>
2023-05-11 07:38:14 +0700

    change the source photo to use cloud storage

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/dicoding-gce-course/**",
+      },
+    ],
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,12 +78,19 @@ const Home: NextPage = () => {
                 borderRadius="full"
                 overflow="hidden"
               >
-                <ProfileImage
-                  src="/images/dimas.jpg"
+                {/* <ProfileImage
+                  // src="/images/dimas.jpg"
+                  src="https://storage.googleapis.com/dicoding-gce-course/dimas.jpg"
                   alt="Profile image"
                   borderRadius="full"
                   width="100%"
                   height="100%"
+                /> */}
+
+                <Image
+                  src="https://storage.googleapis.com/dicoding-gce-course/dimas.jpg"
+                  alt="Profile image"
+                  layout="fill"
                 />
               </Box>
             </Box>
